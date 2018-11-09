6bedb9e419d92e8b056d59a3e00ce4d25ef9f80f
qfstudy <qifeistudy@gmail.com>
2018-11-09 13:34:34 +0800

    parcel打包

---
+++ src/button.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button class="g-button">按钮</button>
+    </div>
+</template>
+
+<script>
+export default{
+}
+   
+</script>
+
+<style lang="scss">
+    .g-button{
+        color: red;
+    }
+</style>
